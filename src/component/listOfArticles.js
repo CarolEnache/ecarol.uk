f5a7256834a22b4c4306fbabf35ec55daf72bc9f
Carol Enache <enache.carol@gmail.com>
2017-11-01 12:55:34 +0000

    adjust various development and production settings by setting environment variables  with .env.

---
+++ src/component/listOfArticles.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './listOfArticles.css';
+
+class ListOfArticles extends Component{
+
+    render(){
+        return(
+            <div>Test</div>
+        )
+    }
+}
+
+export default ListOfArticles;
